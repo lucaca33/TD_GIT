b673a676e20d7594d66edb2671c8dae25f79dc37
Lucas Coelho <lucascoelho2211@gmail.com>
2025-10-13 15:00:23 +0200

    WEEE ARE THE CHAMPIONS MY FRIENDS

--- ex2.c
+++ ex2.c
@@ -11,12 +11,13 @@ int main()
     float t;
     printf("Le taux anuel : ");
     scanf("%f", &t);
+    t = t/100;
     getchar();
     int n;
     printf("La duree du pret en annees : ");
     scanf("%d", &n);
 
-    float mensualite = (C * (t / 12)) / (1 - (pow(1 + (t / 12), n * 12)));
+    float mensualite = (C * (t / 12)) / (1 - (pow(1 + (t / 12), -n * 12)));
 
     printf("La mensualite du pret est de : %f\n",mensualite);
     return 0;
