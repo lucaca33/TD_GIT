34f1cd865e62b659e06226faf8092869d4062383
Lucas Coelho <lucascoelho2211@gmail.com>
2025-10-13 13:55:03 +0200

    exo 1 fini

---
+++ ex1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    int sec;
+    printf("combien de secondes ? ");
+    scanf("%d",&sec);
+    int h = sec /3600;
+    int m = (sec-h*3600)/60;
+    int s = (sec-h*3600-m*60);
+    printf("%d secondes = %d heures, %d minutes, %d secondes", sec , h, m, s);
+    return 0;
+}
