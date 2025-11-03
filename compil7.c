0a2382f028b356182d9909cbdaada64cc05abfd2
Lucas Coelho <lucascoelho2211@gmail.com>
2025-11-03 13:57:14 +0100

    corr compil7.c : tableau

--- compil7.c
+++ compil7.c
@@ -3,12 +3,16 @@
 // ici le comilateur ne dit malheureusement rien mais il devrait,
 // executer pour essayer de comprendre ce qu'il se passe 
 int main() {
-    int tableau[5];
+    int tableau[6];
     int i = 12;
-    for (int j = 0; j < 5; j++)
+    for (int j = 0; j < 6; j++)
     {
         tableau[j] = 1;
     }
     printf("OK %d\n", tableau[5]);
     return 0;
 }
+/*
+la variable tableau n'est pas assez grande : il faut lui mettre une taille de 6 et non 5
+pas conséquent, on vas remplacer j < 5 par j < 6
+*/
