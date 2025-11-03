3f3b7d509ca8b5e930f58f5fd513f339343ce277
Lucas Coelho <lucascoelho2211@gmail.com>
2025-11-03 13:45:47 +0100

    correction de compil2.c : suppression des guillemets

--- compil2.c
+++ compil2.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int x = "42";
+    int x = 42;
     printf("%d\n", x);
     return 0;
 }
+/*
+erreur : 
+compil2.c: In function 'main':
+compil2.c:4:13: warning: initialization makes integer from pointer without a cast [-Wint-conversion]
+     int x = "42";
+             ^~~~
+
+correction : j'ai enlevé les guillemets
+*/
