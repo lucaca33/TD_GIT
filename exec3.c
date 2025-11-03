18d8116ac79b549de6a322acc3ee42f5a149ca42
Lucas Coelho <lucascoelho2211@gmail.com>
2025-11-03 14:27:05 +0100

    corr exec3.c

--- exec3.c
+++ exec3.c
@@ -2,7 +2,11 @@
 
 int main() {
     int x;
+    x = 5;
     int y = x + 5;  
     printf("%d\n", y);
     return 0;
 }
+/*
+on initialise pas la variable x et pourtant on l'utilise dans la déclaration de y
+*/
