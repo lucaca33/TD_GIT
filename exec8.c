d24b0b65c4d53bb7314b8c0dd0caba78f84596cb
Lucas Coelho <lucascoelho2211@gmail.com>
2025-11-03 14:40:17 +0100

    cor exec8.c -1

--- exec8.c
+++ exec8.c
@@ -2,7 +2,10 @@
 
 int main() {
     int t[3] = {10, 20, 30};
-    int i = -1;
+    int i = 2;
     printf("%d\n", t[i]); 
     return 0;
 }
+/*
+on peut pas prendre -1 comme index et arriver au dernier élement du tableau en c donc on remplace le -1 par 2
+*/
