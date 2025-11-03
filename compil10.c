fe80bbe4175b44e5e9aedcb7ec1e35c183a07129
Lucas Coelho <lucascoelho2211@gmail.com>
2025-11-03 14:03:56 +0100

    cor compil10.c : abc

--- compil10.c
+++ compil10.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    int 1abc = 5;
-    printf("%d\n", 1abc);
+    int abc = 5;
+    printf("%d\n", abc);
     return 0;
 }
+/*
+compil10.c:4:9: error: invalid suffix "abc" on integer constant
+     int 1abc = 5;
+         ^~~~
+compil10.c:4:9: error: expected identifier or '(' before numeric constant
+compil10.c:5:20: error: invalid suffix "abc" on integer constant
+     printf("%d\n", 1abc);
+                    ^~~~
+
+correction : on peut pas mettre un nombre comme permier charactere d'une variable donc on enleve et remplace la variable par abc
+*/
