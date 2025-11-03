f795fb738e3b0ff452e43a3a50c50ab99ac12cd8
Lucas Coelho <lucascoelho2211@gmail.com>
2025-11-03 13:59:44 +0100

    cor compil9.c : s[6]

--- compil9.c
+++ compil9.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    char s[3] = "Hello";
+    char s[6] = "Hello";
     printf("%s\n", s);
     return 0;
 }
+/*
+erreur ligne 4 : "a value of type "char [6]" cannot be used to initialize an entity of type "char [3]"
+
+donc on met une taille de 6 pour la variable char s
+*/
