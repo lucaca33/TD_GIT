d711e351ad70be55ae7f5239f14bc11346b6c577
Lucas Coelho <lucascoelho2211@gmail.com>
2025-11-03 14:29:11 +0100

    corr exec4.c

--- exec4.c
+++ exec4.c
@@ -4,6 +4,12 @@ int main() {
     int i = 0;
     while (i < 10) {
         printf("%d\n", i);
+        i += 1;
     }
     return 0;
 }
+/*
+la condition de sortie est i >= 10 mais on ne change jamais la valeur de i donc on reste dans la boucle indéfiniment
+
+correction : on ajoute i += 1;
+*/
