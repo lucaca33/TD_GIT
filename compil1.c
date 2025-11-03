e580738fa58f82a83b553e580db74c95f1229a4f
Lucas Coelho <lucascoelho2211@gmail.com>
2025-11-03 13:44:04 +0100

    correction de compil1.c

--- compil1.c
+++ compil1.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello world\n")
+    printf("Hello world\n");
     return 0;
 }
+/*
+erreur ligne 5 : "expected a ';'"
+correction : ajout d'un ';' à la fin de la ligne 4
+*/
