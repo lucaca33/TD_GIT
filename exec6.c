b47499738f25dacf93e73b9651eb4d27b7b70ad1
Lucas Coelho <lucascoelho2211@gmail.com>
2025-11-03 14:34:28 +0100

    corr exec6.c : ajout du &

--- exec6.c
+++ exec6.c
@@ -3,7 +3,10 @@
 int main() {
     char nom[4];
     printf("Nom ? ");
-    scanf("%s", nom); 
+    scanf("%s", &nom); 
     printf("Bonjour %s\n", nom);
     return 0;
 }
+/*
+on ajoute un & avant le nom (ligne 6)
+*/
