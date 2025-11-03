666b6895528fe70c39f52b753589101f436a1d05
Lucas Coelho <lucascoelho2211@gmail.com>
2025-11-03 14:11:14 +0100

    cor compil11 : ajout de { après else

--- compil11.c
+++ compil11.c
@@ -3,8 +3,18 @@
 int main() {
     if (1) {
         printf("ok\n");
-    } else
+    } else {
         printf("nope\n");
         }
     return 0;
 }
+/*
+compil11.c:9:5: error: expected identifier or '(' before 'return'
+     return 0;
+     ^~~~~~
+compil11.c:10:1: error: expected identifier or '(' before '}' token
+ }
+ ^
+
+ corrction : il faut ajouter un { après le else
+*/
