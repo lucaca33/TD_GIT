bcd1d11ec7861ae22582919ba12650b5d123205c
Lucas Coelho <lucascoelho2211@gmail.com>
2025-11-03 13:52:29 +0100

    corr compil5.c : %c

--- compil5.c
+++ compil5.c
@@ -2,6 +2,9 @@
 
 int main() {
     char c = 'A';
-    printf("c = %s\n", c);
+    printf("c = %c\n", c);
     return 0;
 }
+/*
+y'a pas d'erreur de compilation mais j'ai remplacé le %s pas un %c car c = 'A'
+*/
