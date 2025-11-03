e38267f11b83aa663fe482dae59c13f4c33b5f71
Lucas Coelho <lucascoelho2211@gmail.com>
2025-11-03 14:04:27 +0100

    correction du commentaire pour une meilleure lisabilité

--- compil3.c
+++ compil3.c
@@ -11,5 +11,5 @@ compil3.c:5:5: error: 'b' undeclared (first use in this function)
      b = 10;
      ^
 
-correction : int b = 10;
+correction : on met int devant pour bien initialiser la variable int b = 10;
 */
