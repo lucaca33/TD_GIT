e25cf1c2c7974c674fcd23455aca647d7c9c4e2e
Lucas Coelho <lucascoelho2211@gmail.com>
2025-11-17 15:44:58 +0100

    sa marche pas mais c'est pas grave

--- TP3/tp3_morpion.c
+++ TP3/tp3_morpion.c
@@ -4,76 +4,117 @@ int main()
 {
     char plateau[3][3] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
     int joueur = 1;
+    int winner_trouve = 0; // 0 pour non et 1 pour oui
     char coup[4];
-    printf("Joueur %d, entrez votre position (ligne colonne) : ", joueur);
-    fgets(coup, 4, stdin); // sa marche
+    while (winner_trouve == 0)
+    {
+        printf("Joueur %d, entrez votre position (ligne colonne) : ", joueur);
+        fgets(coup, 4, stdin); // sa marche
 
-    char point = ' ';
-    char winner = ' ';
-    for (int i = 0; i < 3; i++)
-    {
-        // les lignes
-        point = plateau[i][0];
-        if (plateau[i][1] == point && plateau[i][0] == point)
+        char point = ' ';
+        char winner = ' ';
+        for (int i = 0; i < 3; i++)
         {
-            winner = point;
-        }
-        // les colonnes
-        point = plateau[0][i];
-        if (plateau[1][i] == point && plateau[1][i] == point)
-        {
-            winner = point;
-        }
-        // diagonale n 1
-        point = plateau[0][0];
-        if (plateau[1][1] == point && plateau[2][2] == point)
-        {
-            winner = point;
-        }
-        // diagonale n 2
-        point = plateau[2][0];
-        if (plateau[1][1] == point && plateau[0][2] == point)
-        {
-            winner = point;
-        }
-    }
-    // on dit égalité
-    if (winner == ' ')
-    {
-        winner = 'e';
-    }
-    
-    
-    // afficher grille morpion
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            if (j != 2)
+            // les lignes
+            if (plateau[i][0] == ' ')
             {
-                printf(" %c |", plateau[i][j]);
+                point = 'r'; // un truc au pif : dans tout les cas il y a pas de gagnants ici
             }
-            else
+            else{
+                point = plateau[i][0] ;
+            }
+
+            if (plateau[i][1] == point && plateau[i][0] == point)
             {
-                printf(" %c\n", plateau[i][j]);
+                winner = point;
+            }
+            // les colonnes
+            if (plateau[0][i] == ' ')
+            {
+                point = 'r'; // un truc au pif : dans tout les cas il y a pas de gagnants ici
+            }
+            else{
+                point = plateau[i][0] ;
+            }
+            if (plateau[1][i] == point && plateau[1][i] == point)
+            {
+                winner = point;
+            }
+            // diagonale n 1
+            if (plateau[0][0] == ' ')
+            {
+                point = 'r'; // un truc au pif : dans tout les cas il y a pas de gagnants ici
+            }
+            else{
+                point = plateau[i][0] ;
+            }
+            if (plateau[1][1] == point && plateau[2][2] == point)
+            {
+                winner = point;
+            }
+            // diagonale n 2
+            if (plateau[2][0] == ' ')
+            {
+                point = 'r'; // un truc au pif : dans tout les cas il y a pas de gagnants ici
+            }
+            else{
+                point = plateau[i][0] ;
+            }
+            if (plateau[1][1] == point && plateau[0][2] == point)
+            {
+                winner = point;
             }
         }
-        if (i != 2)
+        // on dit égalité
+        if (winner == 'O' || winner == 'X')
         {
-            for (int j = 0; j < 11; j++)
+            winner_trouve = 1; // sa vas sortir de la boucle
+        }
+
+        int case_restante = 0;
+        // case jouable check, sinon egalité
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
             {
-                if (i == 4 || i == 7)
+                if (plateau[i][j] == ' ')
                 {
-                    printf("|");
+                    winner_trouve = 1; // sa vas sortir de la boucle
+                }
+            }
+        }
+
+        // afficher grille morpion
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (j != 2)
+                {
+                    printf(" %c |", plateau[i][j]);
                 }
                 else
                 {
-                    printf("-");
+                    printf(" %c\n", plateau[i][j]);
                 }
             }
-        }
+            if (i != 2)
+            {
+                for (int j = 0; j < 11; j++)
+                {
+                    if (i == 4 || i == 7)
+                    {
+                        printf("|");
+                    }
+                    else
+                    {
+                        printf("-");
+                    }
+                }
+            }
 
-        printf("\n");
+            printf("\n");
+        }
     }
 
     return 0;
