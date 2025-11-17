f98d951ed14623776e93dc9212fd936f21b1a081
Lucas Coelho <lucascoelho2211@gmail.com>
2025-11-17 13:32:37 +0100

    correction ordre width et height + pointe de l'étoile

--- TP3/tp3.c
+++ TP3/tp3.c
@@ -1,26 +1,52 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
     int taille;
     printf("Saisissez une taille n (5 <= n <= 10) : ");
     scanf("%d", &taille);
-    int width = taille*taille;
+    int width = taille * taille;
     int max_height = taille * 3 - 1;
-    char img[112][30];
-    for (int i = 0; i < 111; i++)
+    char img[30][122];
+    for (int i = 0; i < 29; i++)
     {
-        for (int j = 0; j < 29; j++)
+        for (int j = 0; j < 121; j++)
         {
             img[i][j] = ' ';
         }
     }
-    for (int i = 0; i < 111; i++)
+
+    int mid = width / 2;
+    for (int i = 0; i < taille; i++)
     {
-        for (int j = 0; j < 29; j++)
+        if (i == taille-1)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                img[i][j] = '_';
+            }
+        }
+        for (int j = 0; j < i * 2 + 1; j++)
+        {
+            if (j == 0 || j == i * 2)
+            {
+                img[i][mid + (j - i)] = 'A';
+            }
+            else
+            {
+                img[i][mid + (j - i)] = 'S';
+            }
+        }
+    }
+
+    for (int i = 0; i < 29; i++)
+    {
+        for (int j = 0; j < 121; j++)
         {
             printf("%c", img[i][j]);
         }
+        printf("\n");
     }
-    
+
     return 0;
 }
