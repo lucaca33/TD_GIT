dd451e9a5d88b0a47673884927e17b38ae3b444a
Lucas Coelho <lucascoelho2211@gmail.com>
2025-11-17 13:06:44 +0100

    mise en place du canvas

---
+++ TP3/tp3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int taille;
+    printf("Saisissez une taille n (5 <= n <= 10) : ");
+    scanf("%d", &taille);
+    int width = taille*taille;
+    int max_height = taille * 3 - 1;
+    char img[112][30];
+
+    
+    return 0;
+}
