64e35ea1aa898a1de139d4bcb5dd0becf6fd086b
Lucas Coelho <lucascoelho2211@gmail.com>
2025-11-17 15:17:37 +0100

    affichage grille morpion

--- TP3/tp3_morpion.c
+++ TP3/tp3_morpion.c
@@ -2,7 +2,38 @@
 
 int main()
 {
-    char plateau[3][3]= {{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}};
-    
+    char plateau[3][3] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+    // afficher grille morpion
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (j != 2)
+            {
+                printf(" %c |", plateau[i][j]);
+            }
+            else
+            {
+                printf(" %c\n", plateau[i][j]);
+            }
+        }
+        if (i != 2)
+        {
+            for (int j = 0; j < 11; j++)
+            {
+                if (i == 4 || i == 7)
+                {
+                    printf("|");
+                }
+                else
+                {
+                    printf("-");
+                }
+            }
+        }
+
+        printf("\n");
+    }
+
     return 0;
 }
