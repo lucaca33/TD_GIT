60086a7862d41af5c1c0ae2c6448347f3a2edb56
Lucas Coelho <lucascoelho2211@gmail.com>
2025-11-17 15:35:29 +0100

    entape 4 et 5 en même temps, c'est plus simple comme sa

--- TP3/tp3_morpion.c
+++ TP3/tp3_morpion.c
@@ -7,6 +7,43 @@ int main()
     char coup[4];
     printf("Joueur %d, entrez votre position (ligne colonne) : ", joueur);
     fgets(coup, 4, stdin); // sa marche
+
+    char point = ' ';
+    char winner = ' ';
+    for (int i = 0; i < 3; i++)
+    {
+        // les lignes
+        point = plateau[i][0];
+        if (plateau[i][1] == point && plateau[i][0] == point)
+        {
+            winner = point;
+        }
+        // les colonnes
+        point = plateau[0][i];
+        if (plateau[1][i] == point && plateau[1][i] == point)
+        {
+            winner = point;
+        }
+        // diagonale n 1
+        point = plateau[0][0];
+        if (plateau[1][1] == point && plateau[2][2] == point)
+        {
+            winner = point;
+        }
+        // diagonale n 2
+        point = plateau[2][0];
+        if (plateau[1][1] == point && plateau[0][2] == point)
+        {
+            winner = point;
+        }
+    }
+    // on dit égalité
+    if (winner == ' ')
+    {
+        winner = 'e';
+    }
+    
+    
     // afficher grille morpion
     for (int i = 0; i < 3; i++)
     {
