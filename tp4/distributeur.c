a3d92921510337b777fef97b70686fa3b5966001
Lucas Coelho <lucascoelho2211@gmail.com>
2025-11-24 14:56:48 +0100

    afficher menu / etape 1

--- tp4/distributeur.c
+++ tp4/distributeur.c
@@ -1,1 +1,32 @@
 #include <stdio.h>
+
+void afficherMenu()
+{
+    printf("===== Banque - Distributeur =====\n");
+    printf("1. Faire un retrait\n");
+    printf("2. Quitter\n");
+}
+int lireChoix(){
+    printf("Votre choix : ");
+    int temp = 0;
+    scanf("%d", &temp);
+}
+
+int main()
+{
+    int choix = -1;
+    while (choix != 0)
+    {
+        afficherMenu();
+        choix = lireChoix();
+        printf("================\n");
+        switch (choix)
+        {
+        case 1:
+            break;
+        case 2:
+            break;
+        }
+    }
+    return 0;
+}
