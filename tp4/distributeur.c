73df3402488e102588f84f077c6bb544afcfe701
Lucas Coelho <lucascoelho2211@gmail.com>
2025-11-24 15:14:38 +0100

    fin

--- tp4/distributeur.c
+++ tp4/distributeur.c
@@ -7,20 +7,23 @@ void afficherMenu()
     printf("2. Quitter\n");
 }
 
-int lireChoix(){
+int lireChoix()
+{
     printf("Votre choix : ");
     int temp = 0;
     scanf("%d", &temp);
 }
 
-int saisir_montant(){
+int saisir_montant()
+{
     printf("Montant a retirer : ");
     int montant = 0;
     scanf("%d", montant);
     return montant;
 }
 
-int montant_valide(int montant){
+int montant_valide(int montant)
+{
     if (montant % 5 != 0)
     {
         printf("Erreur : montant invalide.\nLe montant doit etre en 5 et 1000 euros, divisible pas 5.");
@@ -29,11 +32,34 @@ int montant_valide(int montant){
     return 0;
 }
 
+void calcul_distribution(int montant)
+{
+    int nb50, nb20, nb5 = 0;
+    while (montant >= 50)
+    {
+        nb50++;
+        montant -= 50;
+    }
+    while (montant >= 20)
+    {
+        nb20++;
+        montant -= 20;
+    }
+    while (montant >= 5)
+    {
+        nb5++;
+        montant -= 5;
+    }
+    printf("%d billets de 50 euros\n", nb50);
+    printf("%d billets de 20 euros\n", nb20);
+    printf("%d billets de 5 euros\n", nb5);
+}
 
 int main()
 {
     int choix = -1;
-    while (choix != 0)
+    int montant;
+    while (choix != 2)
     {
         afficherMenu();
         choix = lireChoix();
@@ -41,8 +67,11 @@ int main()
         switch (choix)
         {
         case 1:
-            break;
-        case 2:
+            montant = saisir_montant();
+            if (montant_valide(montant))
+            {
+                calcul_distribution(montant);
+            }
             break;
         }
     }
