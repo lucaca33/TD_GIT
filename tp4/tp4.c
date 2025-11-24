93de219a3d25428491e80648f54b36e92e496b24
Lucas Coelho <lucascoelho2211@gmail.com>
2025-11-24 12:56:19 +0100

    fonction afficherMenu

---
+++ tp4/tp4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void afficherMenu(){
+    printf("===== GESTION DES NOTES =====\n");
+    printf("1. Saisir le nombre d'eleves\n");
+    printf("2. Saisir les notes des eleves\n");
+    printf("3. Afficher toutes les notes\n");
+    printf("4. Afficher le moyenne d'un eleve\n");
+    printf("5. Afficher le moyenne generale\n");
+    printf("6. Afficher le meilleure note de chaque controle\n");
+    printf("0. Quitter\n");
+}
+
+int main(){
+    return 0;
+}
