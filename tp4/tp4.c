5dd91540f190c5398f96d59cb0d8e1dc607ac21a
Lucas Coelho <lucascoelho2211@gmail.com>
2025-11-24 13:04:55 +0100

    fonction saisirNombreEleves

--- tp4/tp4.c
+++ tp4/tp4.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-void afficherMenu(){
+void afficherMenu()
+{
     printf("===== GESTION DES NOTES =====\n");
     printf("1. Saisir le nombre d'eleves\n");
     printf("2. Saisir les notes des eleves\n");
@@ -11,15 +12,32 @@ void afficherMenu(){
     printf("0. Quitter\n");
 }
 
-int lireChoix(){
+int lireChoix()
+{
     printf("Votre choix : ");
     int temp;
     scanf("%d", &temp);
     return temp;
 }
 
-int main(){
+int saisirNombreEleves()
+{
+    printf("Entrez le nombre d'eleves (1 a 30) : ");
+    int nb;
+    scanf("%d", &nb);
+    while (nb < 1 || nb > 30)
+    { // note : on aurait pu utiliser un do pour faire plus joli
+        printf("Valeur invalide.\n");
+        printf("Entrez le nombre d'eleves (1 a 30) : ");
+        scanf("%d", &nb);
+    }
+    return nb;
+}
+
+int main()
+{
     afficherMenu();
     lireChoix();
+    saisirNombreEleves();
     return 0;
 }
