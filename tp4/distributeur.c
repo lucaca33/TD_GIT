540eca2e8a41cca0638956ce0f95d9ce72cf48bd
Lucas Coelho <lucascoelho2211@gmail.com>
2025-11-24 15:35:47 +0100

    ajout du header pour le distributeur de billet

--- tp4/distributeur.c
+++ tp4/distributeur.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "distributeur.h"
 
 void afficherMenu()
 {
---
+++ tp4/distributeur.h
@@ -0,0 +1,5 @@
+void afficherMenu();
+int lireChoix();
+int saisir_montant();
+int montant_valide(int montant);
+void calcul_distribution(int montant);
